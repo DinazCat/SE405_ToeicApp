948c086cb9793e1534c92f930bc7d4c867622756
21522288 <21522288@gm.uit.edu.vn>
2024-03-20 22:00:14 +0700

    add create, join meeting fuction

---
+++ api/apiVideoSDK.js
@@ -0,0 +1,15 @@
+export const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiJhMzgxYWJlMS01ODc1LTQwNDEtOGY2ZC0wOTdkMzQ2NDNkM2EiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTcxMDU5MTkyNiwiZXhwIjoxODY4Mzc5OTI2fQ.O-nDX7-tEHkuYXIosxmmFqUcyfdM97VhJOgUrlMl5Zw";
+// API call to create meeting
+export const createMeeting = async ({ token }) => {
+  const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
+    method: "POST",
+    headers: {
+      authorization: `${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}),
+  });
+
+  const { roomId } = await res.json();
+  return roomId;
+};
--- index.js
+++ index.js
@@ -7,6 +7,9 @@ import App from './App';
 import {name as appName} from './app.json';
 import OnboardingScreen from './screens/OnboardingScreen';
 import Home from './screens/Home';
+import { register } from "@videosdk.live/react-native-sdk";
+
+register();
 
 console.disableYellowBox = true; 
 console.warn = () => {}; 
